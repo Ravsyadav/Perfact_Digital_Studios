237147eb63b62267b93e9ec2cb13d1f2f4d6b47c
Ravsyadav <ravsydv@gmail.com>
2026-05-01 02:34:27 +0530

    Gallery Update

--- src/routes/Gallery.jsx
+++ src/routes/Gallery.jsx
@@ -9,10 +9,19 @@ import { images } from '../assets/images/images';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css';
+import 'lightgallery/css/lg-fullscreen.css';
+import 'lightgallery/css/lg-share.css';
+import 'lightgallery/css/lg-rotate.css';
+
 
 // import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay';
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
 
 
 const Gallery = () => {
@@ -27,7 +36,7 @@ const Gallery = () => {
             <LightGallery
                 onInit={onInit}
                 speed={500}
-                plugins={[lgThumbnail, lgZoom]}
+                plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgShare, lgRotate]}
             >
               {
                 images.map((image, idx)=>{
